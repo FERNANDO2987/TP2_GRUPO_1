4c14d3a81f210a00c4898375d6cdad0b6f6d3e63
Santiago Escobar <santiago.escobar97@gmail.com>
2024-08-20 20:41:03 -0300

    Punto 2 y 3

--- src/Ejercicio1/Profesor.java
+++ src/Ejercicio1/Profesor.java
@@ -1,6 +1,43 @@
 package Ejercicio1;
 
 public class Profesor extends Empleado{
+	
 	private String cargo;
 	private int antiguedad;
+	
+	// Constructores
+	public Profesor()
+	{
+		super();
+		cargo = "Sin cargo";
+		antiguedad = 0;
+	}
+	
+	public Profesor(String nombre, int edad, String cargo, int antiguedad)
+	{
+		super(nombre, edad);
+		this.cargo = cargo;
+		this.antiguedad = antiguedad;
+	}
+	
+	// Getters & Setters
+	public String getCargo() 
+	{
+		return cargo;
+	}
+	
+	public void setCargo(String cargo) 
+	{
+		this.cargo = cargo;
+	}
+		
+	public int getAntiguedad() 
+	{
+		return antiguedad;
+	}
+	
+	public void setAntiguedad(int antiguedad) 
+	{
+		this.antiguedad = antiguedad;
+	}
 }
