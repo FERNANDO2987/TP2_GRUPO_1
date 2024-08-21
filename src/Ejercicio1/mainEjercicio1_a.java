2e72761b988e45e2018b9d9b79cc55693f4e4a4a
Fernando2987 <fernandopalacios51@gmail.com>
2024-08-20 21:43:26 -0300

    Add: Punto 5
    Crear una clase llamada mainEjercicio1_a, donde se creen 5 profesores y se guarden dentro de un arraylist. Mostrar la información utilizando un iterador.

---
+++ src/Ejercicio1/mainEjercicio1_a.java
@@ -0,0 +1,28 @@
+package Ejercicio1;
+import java.util.ArrayList;
+import java.util.Iterator;
+public class mainEjercicio1_a {
+
+	public static void main(String[] args) {
+		
+		
+		
+        ArrayList<Profesor> listaProfesores = new ArrayList<>();
+
+        
+        listaProfesores.add(new Profesor("Juan Pérez", 45, "Titular", 20));
+        listaProfesores.add(new Profesor("María González", 38, "Suplente", 15));
+        listaProfesores.add(new Profesor("Carlos López", 50, "Titular", 25));
+        listaProfesores.add(new Profesor("Ana Martínez", 42, "Ayudante", 18));
+        listaProfesores.add(new Profesor("Luis Fernández", 30, "Jefe de Trabajos Prácticos", 8));
+
+       
+        Iterator<Profesor> iterador = listaProfesores.iterator();
+        while (iterador.hasNext()) {
+            System.out.println(iterador.next());
+		
+
+        }
+	}
+
+	}
