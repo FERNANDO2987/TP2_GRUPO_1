5bb13a7482aa328683771d09d1e7e94e0b270b40
Alejo Chavez <alejo.chavez@alumnos.frgp.utn.edu.ar>
2024-08-25 22:44:15 -0300

    Clases que se borraron

---
+++ Ejercicio1/src/ejercicio2/ProductoRefrigerado.java
@@ -0,0 +1,27 @@
+package ejercicio2;
+
+public class ProductoRefrigerado extends Producto {
+	private String codigoOrgSupAlimentaria;
+
+	public ProductoRefrigerado() {
+		super();
+	}
+
+	public ProductoRefrigerado(String fechaCaducidad, int nroLote, String codigoOrgSupAlimentaria) {
+		super(fechaCaducidad, nroLote);
+		this.codigoOrgSupAlimentaria = codigoOrgSupAlimentaria;
+	}
+
+	public String getCodigoOrgSupAlimentaria() {
+		return codigoOrgSupAlimentaria;
+	}
+
+	public void setCodigoOrgSupAlimentaria(String codigoOrgSupAlimentaria) {
+		this.codigoOrgSupAlimentaria = codigoOrgSupAlimentaria;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " codigoOrgSupAlimentaria=" + codigoOrgSupAlimentaria;
+	}	
+}
