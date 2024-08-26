4645633c53296b8447f085efebbf92aa7c7a582d
Bassinii <brunobassini86@gmail.com>
2024-08-26 13:45:23 -0300

    Add:Clases y interfaces ejercicio 3

---
+++ Ejercicio1/src/ejercicio3/EdificioDeOficinas.java
@@ -0,0 +1,14 @@
+package ejercicio3;
+
+public class EdificioDeOficinas implements IEdificio {
+	int nroOficinas;
+	double superficie;
+	
+	public EdificioDeOficinas() {
+		
+	}
+	
+	public double getSuperficieEdificio() {
+		return superficie;
+	}
+}
---
+++ Ejercicio1/src/ejercicio3/Polideportivo.java
@@ -0,0 +1,38 @@
+package ejercicio3;
+
+public class Polideportivo implements IEdificio,IInstalacionDeportiva {
+	private String nombre;
+	private double superficie;
+	private int tipoDeInstalacion;
+	
+	public Polideportivo() {
+		
+	}
+	
+	public double getSuperficieEdificio() {
+		return superficie;
+	}
+	
+	public int getTipoDeInstalacion() {
+		return tipoDeInstalacion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setSuperficie(double superficie) {
+		this.superficie = superficie;
+	}
+
+	public void setTipoDeInstalacion(int tipoDeInstalacion) {
+		this.tipoDeInstalacion = tipoDeInstalacion;
+	}
+	
+	
+	
+}
