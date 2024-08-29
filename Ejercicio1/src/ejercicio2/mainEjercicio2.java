bb5ae81bb93704f976e0303367d79c57abe4f9b0
Fernando2987 <fernandopalacios51@gmail.com>
2024-08-29 18:07:31 -0300

    change: camibios

--- Ejercicio1/src/ejercicio2/mainEjercicio2.java
+++ Ejercicio1/src/ejercicio2/mainEjercicio2.java
@@ -5,16 +5,16 @@ public class mainEjercicio2 {
 	public static void main(String[] args) {
 		
 
-		  // Crear un objeto ProductoCongelado  
+		 
         ProductoCongelado productoCongelado = new ProductoCongelado("2025-12-31", 1001, -18.0f);  
         
-        // Crear un objeto ProductoFresco  
+        
         ProductoFresco productoFresco = new ProductoFresco("2023-09-30", 2002, "2023-08-01", "España");  
         
-        // Crear un objeto ProductoRefrigerado  
+        
         ProductoRefrigerado productoRefrigerado = new ProductoRefrigerado("2024-06-15", 3003, "A1234");  
 
-        // Mostrar los datos de cada producto  
+         
         System.out.println(productoCongelado);  
         System.out.println(productoFresco);  
         System.out.println(productoRefrigerado);  
